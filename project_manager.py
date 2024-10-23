4fa599011fdf53401ee393506cd71bf4b85e5ed0
proletsoz <proletsoz@gmail.com>
2024-10-23 15:36:12 -0400

    Create directory is success.

--- project_manager.py
+++ project_manager.py
@@ -1,7 +1,12 @@
-import os
 from pathlib import Path
+
 
 class Project:
     def __init__(self, name: str, path: Path):
         self.name = name
         self.path = path
+
+    def create_directory(self):
+        if self.path.exists():
+            raise FileExistsError(f"{self.name} уже существует!")
+        self.path.mkdir(parents=True)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import shutil
 import unittest
 from pathlib import Path
 
@@ -9,4 +10,15 @@ class TestProjectManager(unittest.TestCase):
         self.test_name = "test_project"
         self.test_path = Path("./test_project")
         self.test_project = Project(self.test_name, self.test_path)
-    
+
+    def tearDown(self):
+        if self.test_project.path.exists():
+            shutil.rmtree(self.test_project.path)
+
+    def test_create_directory_success(self):
+        self.test_project.create_directory()
+        self.assertTrue(self.test_project.path.exists())
+
+
+if __name__ == "__main__":
+    unittest.main()
