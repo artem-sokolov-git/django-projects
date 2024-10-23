a5ed5b10ff7dc6e9b8d77a30f9783926a17d8fad
proletsoz <proletsoz@gmail.com>
2024-10-23 15:56:20 -0400

    Create virtualenv is succeeded.

--- project_manager.py
+++ project_manager.py
@@ -1,12 +1,20 @@
 from pathlib import Path
+import subprocess
 
 
 class Project:
     def __init__(self, name: str, path: Path):
         self.name = name
         self.path = path
+        self.venv = self.path / ".venv"
 
     def create_directory(self):
         if self.path.exists():
             raise FileExistsError(f"{self.name} уже существует!")
+        # ? Опционально: Создать проверку на корректное имя
         self.path.mkdir(parents=True)
+
+    def create_virtualenv(self):
+        if self.venv.exists():
+            raise FileExistsError("Виртуальное окружение уже создано!")
+        subprocess.run(['python', '-m', 'venv', self.venv])
--- test.py
+++ test.py
@@ -16,8 +16,28 @@ class TestProjectManager(unittest.TestCase):
             shutil.rmtree(self.test_project.path)
 
     def test_create_directory_success(self):
+        """Тест на успешное создание папки"""
         self.test_project.create_directory()
         self.assertTrue(self.test_project.path.exists())
+
+    # ? Опционально: Создать проверку на корректное имя
+
+    def test_create_directory_already_exists(self):
+        """Тест на выброс исключения, если папка уже существует"""
+        self.test_project.create_directory()
+        with self.assertRaises(FileExistsError):
+            self.test_project.create_directory()
+
+    def test_create_virtualenv_success(self):
+        self.test_project.create_directory()
+        self.test_project.create_virtualenv()
+        self.assertTrue(self.test_project.venv.exists())
+
+    def test_create_virtualenv_already_exists(self):
+        """Тест на выброс исключения, если .venv уже существует"""
+        self.test_project.create_virtualenv()
+        with self.assertRaises(FileExistsError):
+            self.test_project.create_virtualenv()
 
 
 if __name__ == "__main__":
